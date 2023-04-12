7a387511754ec2ab1cad161860aab55cba7c94ce
imac <frank.siyli@gmail.com>
2023-04-12 11:29:21 +0200

    change about text

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,14 +7,15 @@ export default function About() {
           <p>
             After nearly two decades working in the automotive industry, I
             decided it was time for a change. I wanted to explore my passion for
-            technology and expand my skillset. That's why I enrolled in the Web
-            Dev bootcamp at "neueFische". The experience was nothing short of
-            transformative and gave me the perfect start into the world of IT.
-            Starting from mid-May 2023, I am excited to join a team where I can
-            continue to grow and contribute to something new. As a skilled
-            craftsman, I pride myself on my hands-on approach to problem-solving
-            and am committed to delivering high-quality results. I am eager to
-            learn from others and bring my unique perspective to any project.
+            technology and expand my skillset. That&apos;s why I enrolled in the
+            Web Dev bootcamp at &quot;neueFische&quot;. The experience was
+            nothing short of transformative and gave me the perfect start into
+            the world of IT. Starting from mid-May 2023, I am excited to join a
+            team where I can continue to grow and contribute to something new.
+            As a skilled craftsman, I pride myself on my hands-on approach to
+            problem-solving and am committed to delivering high-quality results.
+            I am eager to learn from others and bring my unique perspective to
+            any project.
           </p>
         </div>
       </div>
