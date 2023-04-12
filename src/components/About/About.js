d67840ca69d73d121289a64054ab911cd99a1db3
imac <frank.siyli@gmail.com>
2023-04-12 11:28:28 +0200

    change about text

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,12 +5,16 @@ export default function About() {
       <div className="flex-about">
         <div className="about-text">
           <p>
-            After almost two decades in the automotive industry, it was time for
-            a change. Something with new stimuli and enough opportunities to
-            develop myself further. The bootcamp at &quot;neueFische&quot; was
-            the perfect start into the IT-world and from mid of may 2023 I am
-            looking for a team where I can increase my knowledge and be a part
-            of something new.
+            After nearly two decades working in the automotive industry, I
+            decided it was time for a change. I wanted to explore my passion for
+            technology and expand my skillset. That's why I enrolled in the Web
+            Dev bootcamp at "neueFische". The experience was nothing short of
+            transformative and gave me the perfect start into the world of IT.
+            Starting from mid-May 2023, I am excited to join a team where I can
+            continue to grow and contribute to something new. As a skilled
+            craftsman, I pride myself on my hands-on approach to problem-solving
+            and am committed to delivering high-quality results. I am eager to
+            learn from others and bring my unique perspective to any project.
           </p>
         </div>
       </div>
