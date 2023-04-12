207dbc3d2417b1a8886e5dd0204e104f0b03bb60
imac <frank.siyli@gmail.com>
2023-04-12 13:12:13 +0200

    add new CV

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function About() {
   return (
     <div aria-label="About Me" className="about-container">
@@ -18,6 +21,54 @@ export default function About() {
             any project.
           </p>
         </div>
+      </div>
+      <div className="social_icons">
+        <a
+          className="icon_hide"
+          href="https://github.com/FrankSiyli"
+          aria-label="GitHub"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <p className="hide">Github</p>
+          <Image
+            src="/images/github-icon.png"
+            alt="github"
+            width={100}
+            height={100}
+          ></Image>
+        </a>
+
+        <a
+          className="icon_hide"
+          href="https://www.linkedin.com/in/frank-siyli-45b154226/"
+          aria-label="LinkedIn"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <p className="hide">Linkedin</p>
+          <Image
+            src="/images/linkedin-icon.png"
+            alt="linkedin"
+            width={100}
+            height={100}
+          ></Image>
+        </a>
+        <a
+          className="icon_hide"
+          href="/CV"
+          aria-label="CV"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <p className="hide">CV</p>
+          <Image
+            src="/images/resume.png"
+            alt="CV"
+            width={90}
+            height={90}
+          ></Image>
+        </a>
       </div>
     </div>
   );
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,58 +1,7 @@
-import Image from "next/image";
-import Link from "next/link";
-
 export default function Footer() {
   return (
     <>
       <div className="footer-container">
-        <div className="social_icons">
-          <a
-            className="icon_hide"
-            href="https://github.com/FrankSiyli"
-            aria-label="GitHub"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <p className="hide">Github</p>
-            <Image
-              src="/images/github-icon.png"
-              alt="github"
-              width={100}
-              height={100}
-            ></Image>
-          </a>
-
-          <a
-            className="icon_hide"
-            href="https://www.linkedin.com/in/frank-siyli-45b154226/"
-            aria-label="LinkedIn"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <p className="hide">Linkedin</p>
-            <Image
-              src="/images/linkedin-icon.png"
-              alt="linkedin"
-              width={100}
-              height={100}
-            ></Image>
-          </a>
-          <a
-            className="icon_hide"
-            href="/CV"
-            aria-label="CV"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <p className="hide">CV</p>
-            <Image
-              src="/images/resume.png"
-              alt="CV"
-              width={90}
-              height={90}
-            ></Image>
-          </a>
-        </div>
         <div className="footer-impressum">
           <p style={{ color: "white" }}>
             © {new Date().getFullYear()} Frank Siyli
